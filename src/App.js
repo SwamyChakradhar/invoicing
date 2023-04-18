73731216f070284d3ff26e61771fa5fc2f802dc3
Chakradhar <chakradhar.chipuri@fridaymediagroup.com>
2023-04-18 06:20:48 +0530

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import Clients from "./components/pages/Clients";
+import Units from './components/pages/Units';
+import Navbar from './components/layout/Navbar';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello World</h1>
+      <Navbar />
+        <Routes>
+          <Route exact path='/clients' element={<Clients/>} />
+          <Route exact path='/units' element={<Units/>} />
+        </Routes>
     </div>
   );
 }
---
+++ src/components/pages/Units.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Units = () => {
+    return (
+        <div className="container">
+            <div className="py-4">
+                <h1>Units</h1>
+            </div>
+        </div>
+    )
+};
+
+export default Units;
